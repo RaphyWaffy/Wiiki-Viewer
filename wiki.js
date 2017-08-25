6e25b5de1fa009d1f256dfaa73671aec7aeab077
Raphael <reubanaitor@hotmail.com>
2017-08-25 15:52:38 +0100

    Updatated

--- wiki.js
+++ wiki.js
@@ -2,28 +2,29 @@
 /*global $, jQuery, alert*/
 $(document).ready(function() {
   'use strict';
-  $('#mysearch').keypress(function(e){
-        if(e.which == 13){//Enter key pressed
-            $('#look').click();//Trigger search button click event
-        }
-    });
+  $('#mysearch').keypress(function(e) {
+    if (e.which == 13) { //Enter key pressed
+      $('#look').click(); //Trigger search button click event
+    }
+  });
 
   $("#look").click(function() {
     var search = $("#mysearch").val();
     var wikiPage = "https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch=" + search + "&prop=revisions&rvprop=content&format=json&callback=?";
-    console.log(wikiPage);
+
     $.getJSON(wikiPage, function(data) {
       var totalhits = data.query.searchinfo.totalhits;
-      console.log(totalhits);
-        $('#master').empty();
+      $('#master').empty();
+
+      if (data.query.searchinfo.totalhits === 0) {
+        $('#master').append('<div class="head"><h1>The page ' + search + ' does not exist. You can ask for it to be created.There were no results matching the query.</h1></div>')
+      }
+
       for (var i = 0; i < 10 && i < totalhits; i++) {
-
-
-        if (data.query.search == "") {
+        if (data.query.searchinfo.totalhits === 0) {
           $('#master').append('<div class="head"><h1>The page ' + search + ' does not exist. You can ask for it to be created.There were no results matching the query.</h1></div>')
           break;
         } else {
-console.log(data.query.search[i].title);
           var title = data.query.search[i].title.replace(/\s+/g, '');
           var ref = "<a href=" + 'https://en.wikipedia.org/wiki/' + title + ">" + data.query.search[i].title + "</a></br>";
           $("#master").hide().append("<div class='results'>" + ref + data.query.search[i].snippet + "..." + "</div>").fadeIn(400);
@@ -31,7 +32,6 @@ console.log(data.query.search[i].title);
       };
 
       if (totalhits > 10) {
-        console.log("rgregegr");
         $('#master').append('<div class="head"><a href="https://en.wikipedia.org/w/index.php?search=' + search + '&title=Special:Search&profile=default&fulltext=1&searchToken=3ddfhnvbpt5er6plxi6b8ng65"><h1>More Results</h1></a></div>')
 
 
